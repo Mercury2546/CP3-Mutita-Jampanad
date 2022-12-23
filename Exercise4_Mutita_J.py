63ad6d53a0c3fb691352116bb4c1e2c55da3db32
MDoubleT <mutitajampanad@gmail.com>
2022-12-23 15:08:14 +0700

    First Commit
    Exercise 4

---
+++ Exercise4_Mutita_J.py
@@ -0,0 +1,10 @@
+foundScore = 22.5
+genScore = 28
+introScore = 23.7
+comScore = 32.3
+
+print("---Your Score---")
+print("Foundation English :", foundScore)
+print("General Business :", genScore)
+print("Introduction to Computer System :", introScore)
+print("Computer Programming :", comScore)
